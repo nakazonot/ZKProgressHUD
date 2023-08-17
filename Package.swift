33a34fd26685b0e1499210ec205a196c57457f45
nakazonot <nakazono@amatelus.co.jp>
2023-08-17 18:20:01 +0900

    git add Package.swift

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "ZKProgressHUD",
+    products: [
+        .library(name: "ZKProgressHUD", targets: ["ZKProgressHUD"])
+    ],
+    targets: [
+        .target(name: "ZKProgressHUD", dependencies: [], path: ".", sources: ["ZKProgressHUD"])
+    ],
+    swiftLanguageVersions: [.v5]
+)
